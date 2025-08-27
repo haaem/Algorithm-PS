10542085c0c221ecdda395fb2ec15043ec20de64
haaem <serrysallylee@gmail.com>
2025-08-28 00:17:15 +0900

    BJ: 25/08/27

---
+++ BJ/xxxx/2075.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BJ2075 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				pq.add(Integer.parseInt(st.nextToken()));
+			}
+		}
+		for (int i = 0; i < N - 1; i++) {
+			pq.remove();
+		}
+		System.out.println(pq.peek());
+	}
+}
