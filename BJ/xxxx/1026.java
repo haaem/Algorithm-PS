996b38b8c6c8cf5ce5977c50fde12fde11241f38
Hyemin Lee <113160789+haaem@users.noreply.github.com>
2025-09-30 23:00:45 +0900

    BJ: 25/09/30

---
+++ BJ/xxxx/1026.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ1026 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		int[] B = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(A);
+		Arrays.sort(B);
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			sum += A[i] * B[N - 1 - i];
+		}
+		System.out.println(sum);
+	}
+}
