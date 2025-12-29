644b591f2d451cea38d301517033d42850e11d87
haaem <serrysallylee@gmail.com>
2025-12-29 21:49:15 +0900

    BJ: 25/12/29

---
+++ BJ/xxxx/2739.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ2739 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= 9; i++) {
+			System.out.printf("%d * %d = %d\n", N, i, N * i);
+		}
+	}
+}
