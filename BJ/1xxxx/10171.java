1eaddfb29a1ab0448c6988a95ccd61947ea400ed
이혜민 <serrysallylee@gmail.com>
2026-02-09 22:48:13 +0900

    BJ: 26/02/09

---
+++ BJ/1xxxx/10171.java
@@ -0,0 +1,9 @@
+
+public class BJ10171 {
+	public static void main(String[] args) throws Exception {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
