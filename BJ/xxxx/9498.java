b60223c11cb552bbd7020311559f02aab68eab54
이혜민 <serrysallylee@gmail.com>
2026-02-13 23:34:05 +0900

    BJ: 26/02/13

---
+++ BJ/xxxx/9498.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ9498 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		switch (n / 10) {
+		case 10:
+		case 9:
+			System.out.println("A");
+			break;
+		case 8:
+			System.out.println("B");
+			break;
+		case 7:
+			System.out.println("C");
+			break;
+		case 6:
+			System.out.println("D");
+			break;
+		default:
+			System.out.println("F");
+		}
+	}
+}
