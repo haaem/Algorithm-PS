9c944036d31dfd00aedb4280230f7221006d9454
haaem <serrysallylee@gmail.com>
2026-03-03 17:54:55 +0900

    BJ: 26/03/03

---
+++ BJ/1xxxx/10951.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ10951 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		String input;
+		while ((input = br.readLine()) != null) {
+			st = new StringTokenizer(input);
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			System.out.println(A + B);
+		}
+	}
+}
