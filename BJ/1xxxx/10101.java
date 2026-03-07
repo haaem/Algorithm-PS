fdf26780f11ef3305dcac1446d8c7aeb32b27ccf
이혜민 <serrysallylee@gmail.com>
2026-03-07 22:33:38 +0900

    BJ: 26/03/07

---
+++ BJ/1xxxx/10101.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ10101 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+		if (a == 60 && b == 60 && c == 60)
+			System.out.println("Equilateral");
+		else if (a + b + c != 180)
+			System.out.println("Error");
+		else if (a != b && b != c && c != a)
+			System.out.println("Scalene");
+		else
+			System.out.println("Isosceles");
+	}
+}
