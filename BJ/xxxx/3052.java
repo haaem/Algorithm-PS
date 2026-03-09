902420554b5a4885eaf99181eb9ab56d1c6eecd1
haaem <serrysallylee@gmail.com>
2026-03-09 20:32:34 +0900

    BJ: 26/03/09

---
+++ BJ/xxxx/3052.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class BJ3052 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < 10; i++) {
+			set.add(Integer.parseInt(br.readLine()) % 42);
+		}
+		System.out.println(set.size());
+	}
+}
