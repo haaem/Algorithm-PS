75f15bfee6debf0bd374f7b8e219b0f07f40ef5f
haaem <serrysallylee@gmail.com>
2026-03-20 18:39:38 +0900

    BJ: 26/03/20

---
+++ BJ/2xxxx/25304.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ25304 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			X -= a * b;
+		}
+		if (X == 0) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	}
+}
