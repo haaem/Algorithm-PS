c059015d107d06124bb6acd629176c91ab6b8eec
Hyemin Lee <113160789+haaem@users.noreply.github.com>
2026-09-23 17:28:19 +0900

    [level 1] Title: 추억 점수, Time: 1.09 ms, Memory: 96 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        HashMap<String, Integer> map = new HashMap();
+        for (int i = 0; i < name.length; i++) 
+            map.put(name[i], yearning[i]);
+        
+        int[] answer = new int[photo.length];
+        for (int i = 0; i < photo.length; i++) {
+            int total = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                if (map.containsKey(photo[i][j]))
+                    total += map.get(photo[i][j]);
+            }
+            answer[i] = total;
+        }
+        
+        return answer;
+    }
+}
